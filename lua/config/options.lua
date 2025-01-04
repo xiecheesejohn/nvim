53bcb59dd9d3f06b88ee0222fad8d058c4ec6e08
cheesejohn <cheesejohn.xie@gmail.com>
2025-01-04 21:28:59 +0800

    python init

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,13 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- when host is windows,set terminal is powershell
+-- 根据操作系统设置默认终端
+if vim.fn.has("win32") == 1 then
+  -- Windows 系统
+  vim.o.shell = "pwsh" -- 或者 "powershell"
+else
+  -- Linux 或其他类 Unix 系统
+  vim.o.shell = "bash"
+end
