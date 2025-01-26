3189a088fb879c2bb485e8569cdc700ad4bd2deb
cheesejohn <cheesejohn.xie@gmail.com>
2025-01-26 23:13:26 +0800

    add base16

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  -- add nvim-base64
+  { "RRethy/nvim-base16" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      --    colorscheme = "base16-gruvbox-dark-soft",
+      colorscheme = "base16-tender",
+    },
+  },
+}
